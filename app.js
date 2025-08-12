da9d68f9a6067da37af550b63d7d8cfa27015170
Grace <gpalombella0@gmail.com>
2025-08-12 16:56:48 +0200

    feat: adjust cards

--- app.js
+++ app.js
@@ -24,23 +24,52 @@ getListings().then(listings => {
       const card = document.createElement('div');
       card.classList.add('job-listing-card');
 
-      const companyLogo = document.createElement('img');
-      companyLogo.src = listing.logo;
-      companyLogo.alt = `${listing.company} logo`;
-      
-      const jobTitle = document.createElement('h2');
-      jobTitle.textContent = listing.position;
-      
-      const companyName = document.createElement('p');
-      companyName.textContent = listing.company;
-      
-      const jobLocation = document.createElement('p');
-      jobLocation.textContent = listing.location;
-      
-      
-      card.append(companyLogo, jobTitle, companyName, jobLocation);
+      const { cardDescription, jobTitle, companyName, jobLocation, companyLogo } = getJobListing(listing, card);
+
+      const cardtagListContainer = getJobTagList(listing);
+
+      cardDescription.appendChild(companyName);
+      cardDescription.appendChild(jobTitle);
+      cardDescription.appendChild(jobLocation);
+
+      card.appendChild(companyLogo);
+      card.appendChild(cardDescription);
+      card.appendChild(cardtagListContainer);
 
       container.appendChild(card);
-      
     });
 });
+
+const getJobListing = (listing, card) => {
+    const companyLogo = document.createElement('img');
+    companyLogo.src = listing.logo;
+    companyLogo.alt = `${listing.company} logo`;
+
+    const cardDescription = document.createElement('div');
+    card.classList.add('job-listing-card-description');
+    const companyName = document.createElement('p');
+    companyName.classList.add('job-listing-card-company-name');
+    companyName.textContent = listing.company;
+
+    const jobTitle = document.createElement('h2');
+    jobTitle.textContent = listing.position;
+
+    const jobLocation = document.createElement('p');
+    jobLocation.textContent = listing.location;
+    return { cardDescription, jobTitle, companyName, jobLocation, companyLogo };
+}
+
+const getJobTagList = (listing) => {
+    const cardtagListContainer = document.createElement('div');
+    cardtagListContainer.classList.add('job-listing-card-tag-list-container');
+    const tags = [listing.role, listing.level, ...(listing.languages || []), ...(listing.tools || [])];
+    const tagList = document.createElement('ul');
+    tags.forEach(tag => {
+        const li = document.createElement('li');
+        li.textContent = tag;
+        tagList.appendChild(li);
+    });
+
+    cardtagListContainer.appendChild(tagList);
+    return cardtagListContainer;
+}
